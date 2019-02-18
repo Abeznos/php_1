0b36decb5d9b96d993e818a809f554f99c8055c7
Abeznos <34938320+Abeznos@users.noreply.github.com>
2019-02-18 20:28:36 +0300

    Исправил ошибки

--- lesson_2/task_6.php
+++ lesson_2/task_6.php
@@ -1,11 +1,13 @@
 <?php
     // Задание 6
-
     function power ($val, $pow) {
-        if ($pow != 1) {
+        
+        if ($pow === 0) {
+            return 1;
+        } else if ($pow != 1) {
             return $val * power($val, $pow - 1);
-        } else {
-            return $val;
+        } else if ($pow < 0) {
+            return power(1/$val, -$pow);
         }
-    }
+    }    
 ?>
