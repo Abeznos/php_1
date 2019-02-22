d44691714435f3e3aa949a2b836f25e869c55ea2
Abeznos <34938320+Abeznos@users.noreply.github.com>
2019-02-23 00:30:26 +0300

    Задание 6

---
+++ lesson_3/task_6.php
@@ -0,0 +1,34 @@
+<?php
+    $menu = [
+        "Home" => "#",
+        "About" => "/about.html",
+        "Catalog" => [
+            "Men" => "/catalog/men.php",
+            "Women" => "/catalog/women.php",
+            "Child" => [
+                "Pants" => "/catalog/child/pants.php",
+                "Shoes" => "/catalog/child/shoes.php"
+            ]
+        ],
+        "Contacts" => "/contacts.php"
+     ];
+
+    function renderMenu($menu) {
+        $result = "<ul class=\"menu\">";
+        
+        foreach($menu as $title => $link) {
+            if (is_array($link)) {
+                $result .= "<li class=\"menu-item submenu\">$title";
+                $result .= renderMenu($link);
+                $result .= "</li>";
+            } else {
+                $result .= "<li class=\"menu-item\"><a href=\"$link\" class=\"menu-item-link\">$title</a></li>";
+            }
+        }
+        
+        $result .= "</ul>";
+        return $result;
+    };
+
+    echo renderMenu($menu);
+?>
