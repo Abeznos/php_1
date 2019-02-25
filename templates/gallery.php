e01e1b32225651bec9a76f3673b749d7601896ea
Abeznos <34938320+Abeznos@users.noreply.github.com>
2019-02-25 15:45:38 +0300

    ДЗ 4

---
+++ templates/gallery.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Галлерея</title>
+</head>
+<body>
+    <div class="gallery-container">
+        <?php require_once __DIR__ . "./../engine/config.php"?>
+    </div>
+    
+</body>
+</html>
